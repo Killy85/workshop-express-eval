67816fac4da214405fdc1b3c839e3045b219b773
Killy85 <goureau.n.a@hotmail.fr>
2018-01-26 15:39:50 +0100

    Exercice 5

--- src/app.js
+++ src/app.js
@@ -11,8 +11,19 @@ app.get('/', function(req, res) {
 app.get('/bingo', function(req, res) {
   fs.readFile('./src/numbers.txt', (err,data)=>{
       if(err){console.log(err);res.send("The server couldn't find files to help your query") }
-      number = data.toString().slice(0,-1).split('\n').join()
-      res.end(number);
+      number = data.toString().slice(0,-1).split('\n')
+      input = req.query['mynumbers']
+      if(input == undefined){
+        res.end('The bingo game is already started and known numbers are '+ number);
+      }else{
+        input = input.split(',')
+        if(input.sort().join(',') === number.sort().join(',')){
+          res.send('Bingo')
+        }else{
+          res.send('The bingo game is already started, sorry your numbers doesn\'t match with known numbers '+ number +' ; so you can not say Bingo')
+        }
+      }
+
   })
 
 });
