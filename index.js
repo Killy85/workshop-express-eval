afc841341a54c2b3def906fa0f3cbe2da7712921
Killy85 <goureau.n.a@hotmail.fr>
2018-01-26 15:01:01 +0100

    Exercise 2

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ let chalk = require('chalk');
 const app = require('./src/app');
 
 // port
-const port = process.env.PORT || 3000;
+const port = process.argv[2] || 3000;
 
 // start server here
 app.listen(port, function() {
   console.log(chalk.green('Hello web server'));
+});
+
+
+app.get('/', function(req, res) {
+  res.sendStatus(200);
 });
